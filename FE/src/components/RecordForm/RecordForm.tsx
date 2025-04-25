7cdcfef140583e86844a7daa78e00bc06cb1536c
tishikawa <tomohiro.ishikawa5211@gmail.com>
2025-04-25 16:17:19 +0900

    create saving record form

---
+++ FE/src/components/RecordForm/RecordForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const RecordForm: React.FC = () => {
+  const [date, setDate] = useState<string>();
+  const [principal, setPrincipal] = useState<string>("");
+  const [rate, setRate] = useState<string>("");
+
+  return (
+    <>
+      <div>
+        <input
+          type="date"
+          value={date}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setDate(e.target.value)
+          }
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          value={principal}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPrincipal(e.target.value)
+          }
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          value={rate}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setRate(e.target.value)
+          }
+        />
+      </div>
+      <h3>{date}:{principal}:{rate}</h3>
+    </>
+  );
+};
+
+export default RecordForm;
