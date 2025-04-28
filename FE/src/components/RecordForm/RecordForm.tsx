7f15d208bc65d3888c8cef336c44dd84acf500a1
tishikawa <tomohiro.ishikawa5211@gmail.com>
2025-04-28 15:10:33 +0900

    prot saving table

--- FE/src/components/RecordForm/RecordForm.tsx
+++ FE/src/components/RecordForm/RecordForm.tsx
@@ -1,41 +1,67 @@
 import { useState } from "react";
 
+type SavingRecord = {
+  date: string;
+  principal: string;
+  rate: string;
+};
 
 const RecordForm: React.FC = () => {
-  const [date, setDate] = useState<string>();
-  const [principal, setPrincipal] = useState<string>("");
-  const [rate, setRate] = useState<string>("");
+  const [savingRecord, setSavingRecord] = useState<SavingRecord[]>([]);
+  const [formData, setFormData] = useState<SavingRecord>({
+    date: "",
+    principal: "",
+    rate: "",
+  });
+
+  const handleAddRecord = () => {
+    setSavingRecord([...savingRecord, formData]);
+    console.log(savingRecord[0]);
+  };
 
   return (
     <>
       <div>
         <input
           type="date"
-          value={date}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setDate(e.target.value)
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          onChange={(e) =>
+            setFormData({ ...formData, principal: e.target.value })
           }
         />
       </div>
       <div>
         <input
           type="number"
-          value={principal}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setPrincipal(e.target.value)
-          }
+          onChange={(e) => setFormData({ ...formData, rate: e.target.value })}
         />
       </div>
       <div>
-        <input
-          type="number"
-          value={rate}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setRate(e.target.value)
-          }
-        />
+        <button onClick={handleAddRecord}>登録</button>
       </div>
-      <h3>{date}:{principal}:{rate}</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>記載日</th>
+            <th>金額（円）</th>
+            <th>利率（%）</th>
+          </tr>
+        </thead>
+        <tbody>
+          {savingRecord.map((record, index) => (
+            <tr key={index}>
+              <td>{record.date}</td>
+              <td>{record.principal}</td>
+              <td>{record.rate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
